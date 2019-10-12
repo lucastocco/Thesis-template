d2971b4b6fe58b2b682c5f5067c32d94c9138a53
lucastocco <lucastocco@outlook.it>
2019-10-12 18:18:44 +0200

    start :)

---
+++ clean.sh
@@ -0,0 +1,5 @@
+echo ----------------------------------------------------------
+echo -e	-' \t \t \t 'Pulizia' \t \t \t '- 
+echo ----------------------------------------------------------
+sudo rm -R *.sc *.acn *.acr *.alg *.aux *.bbl *.bcf *.blg *.glg *.glo *.gls
+sudo rm - R *.ist *.lof *.log *.lot *.run.xml *.synctex* *.toc *-frn.tex
